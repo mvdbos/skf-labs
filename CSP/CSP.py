ea32a54fd8f152a3bc7cfe2debe71ff7959012de
Matthijs van den Bos <matthijs@vandenbos.org>
2020-07-13 15:16:31 +0200

    Fix CSP lab

--- CSP/CSP.py
+++ CSP/CSP.py
@@ -1,9 +1,9 @@
-from flask import Flask, request, url_for, render_template, redirect, make_response
-
+from flask import Flask, request, url_for, render_template, redirect, make_response, escape
 
 app = Flask(__name__, static_url_path='/static', static_folder='static')
 
 app.config['DEBUG'] = True
+
 
 @app.route("/")
 def start():
@@ -13,16 +13,17 @@ def start():
 @app.route("/unprotected", methods=['POST'])
 def unprotected():
     xss = request.form['no_csp']
-    return render_template("index.html", xss = xss)
+    return render_template("index.html", xss=xss)
 
 
 @app.route("/protected", methods=['POST'])
 def protected():
     xss = request.form['with_csp']
-    r = make_response(render_template('index.html', xss = xss))
-    r.headers.set('Content-Security-Policy', "default-src 'self'")
-    r.headers.set('Content-Security-Policy', "script-src 'http://127.0.0.1'")
+    untainted = escape(xss)
+    r = make_response(render_template('index.html', xss=untainted))
+    r.headers.set('Content-Security-Policy', "default-src 'self'; script-src 'self'; frame-ancestors 'self';")
     return r
+
 
 @app.errorhandler(404)
 def page_not_found(e):
